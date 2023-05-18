b1e221bbe0585c474358ebe358f9714595b354a0
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-05-18 06:18:34 -0400

    commented code

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
-﻿using System;
+﻿// where the Console.Write function is located
+using System;
 
+// similar to console.log from JS
 Console.Write("Who would you like to say hello to? ");
 
+// waiting for a response which it will convert to a string saved to the variable called name
 string name = Console.ReadLine();
 
+// if the response for name is not a string with characters
 if (string.IsNullOrWhiteSpace(name))
 {
+    // respond with
     Console.WriteLine("Fine, don't say 'hello'!");
 }
+// otherwise
 else
 {
+    // respond with 
     Console.WriteLine($"Hello, {name}!");
 }
